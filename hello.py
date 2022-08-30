4f2dcc7732e23bf56f9cf21d0ce9e1f2d388c2d6
walexkino <waladino@yahoo.com>
2022-08-30 16:48:55 +0000

    git add fixed

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
 
 # Add two numbers
-sum = float(num1) + float(num2)
+sum7 = float(num1) + float(num2)
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum7))
